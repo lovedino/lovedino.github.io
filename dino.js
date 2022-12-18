20af961661dfbb5e713ce8fdd371e6819440ddb9
jasxyke <90468874+jasxyke@users.noreply.github.com>
2022-12-19 01:25:59 +0800

    firs version

---
+++ dino.js
@@ -0,0 +1,86 @@
+import {
+  incrementCustomProperty,
+  setCustomProperty,
+  getCustomProperty,
+} from "./updateCustomProperty.js"
+
+const dinoElem = document.querySelector("[data-dino]")
+const JUMP_SPEED = 0.47
+const GRAVITY = 0.0015
+const DINO_FRAME_COUNT = 2
+const FRAME_TIME = 100
+
+let isJumping
+let dinoFrame
+let currentFrameTime
+let yVelocity
+let dino
+const mj_dino = `imgs/mj_right.png`
+const xyke_dino = 'imgs/xyke_right.png'
+
+export function setupDino() {
+  isJumping = false
+  dinoFrame = 0
+  currentFrameTime = 0
+  yVelocity = 0
+  setCustomProperty(dinoElem, "--bottom", 0)
+  document.removeEventListener("keydown", onJump)
+  document.addEventListener("keydown", onJump)
+  document.removeEventListener("click", jump)
+  document.addEventListener("click", jump)
+  dino = xyke_dino
+  dinoElem.src = xyke_dino
+}
+
+export function updateDino(delta, speedScale) {
+  handleRun(delta, speedScale)
+  handleJump(delta)
+}
+
+export function getDinoRect() {
+  return dinoElem.getBoundingClientRect()
+}
+
+export function setDinoLose() {
+  //dinoElem.src = "imgs/dino-lose.png"
+}
+
+function handleRun(delta, speedScale) {
+  if (isJumping) {
+    dinoElem.src = dino
+    return
+  }
+
+  if (currentFrameTime >= FRAME_TIME) {
+    dinoFrame = (dinoFrame + 1) % DINO_FRAME_COUNT
+    //dinoElem.src = `imgs/dino-run-${dinoFrame}.png`
+    currentFrameTime -= FRAME_TIME
+  }
+  currentFrameTime += delta * speedScale
+}
+
+function handleJump(delta) {
+  if (!isJumping) return
+
+  incrementCustomProperty(dinoElem, "--bottom", yVelocity * delta)
+
+  if (getCustomProperty(dinoElem, "--bottom") <= 0) {
+    setCustomProperty(dinoElem, "--bottom", 0)
+    isJumping = false
+  }
+
+  yVelocity -= GRAVITY * delta
+}
+
+function onJump(e) {
+  if (e.code !== "Space" || isJumping) return
+
+  yVelocity = JUMP_SPEED
+  isJumping = true
+}
+
+function jump(){
+  if(isJumping) return
+  yVelocity = JUMP_SPEED
+  isJumping = true
+}
